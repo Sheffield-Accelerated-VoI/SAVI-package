ca565a203494923f629deee6bc6e5bada260b137
mark-strong <m.strong@sheffield.ac.uk>
2015-09-24 17:01:52 +0100

    bug fix

--- inst/SAVI/ui.R
+++ inst/SAVI/ui.R
@@ -36,7 +36,7 @@ fluidPage(
                     SAVI - Sheffield Accelerated Value of Information")),
 
   mainPanel(
-    h4("SAVI package version 2.0.9 (2015-08-11)"),
+    h4("SAVI package version 2.0.10 (2015-09-24)"),
     h6(HTML("Copyright &copy; 2015 University of Sheffield")),
 
 
