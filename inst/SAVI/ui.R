c530d9012ff5aed1dbf4630cefbaa8fa4077c7c9
mark-strong <m.strong@sheffield.ac.uk>
2016-05-31 16:34:38 +0100

    date change

--- inst/SAVI/ui.R
+++ inst/SAVI/ui.R
@@ -36,7 +36,7 @@ fluidPage(
                     SAVI - Sheffield Accelerated Value of Information")),
 
   mainPanel(
-    h4("SAVI package version 2.0.15 (2015-12-11)"),
+    h4("SAVI package version 2.0.15 (2016-05-31)"),
     h6(HTML("Copyright &copy; 2015 University of Sheffield")),
 
 
