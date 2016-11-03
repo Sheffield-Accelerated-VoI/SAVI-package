411b708993ac8d9ea42c76404849781bd1ffbe6d
mark-strong <m.strong@sheffield.ac.uk>
2016-11-03 09:05:53 +0000

    bug fix

--- R/SAVI.R
+++ R/SAVI.R
@@ -2,7 +2,7 @@
 SAVI <- function() {
   appDir <- system.file("SAVI", package = "SAVI")
   if (appDir == "") {
-    stop("Could not SAVI directory. Try re-installing `SAVI`.", call. = FALSE)
+    stop("Could not find SAVI directory. Try re-installing `SAVI`.", call. = FALSE)
   }
 
   shiny::runApp(appDir, display.mode = "normal", launch.browser = TRUE)
